83bada498745cf163f505e7ae538cd4842bb5168
megumi <ooo.megumi9@gmail.com>
2023-10-22 09:01:02 +0900

    最寄り駅0件の場合、空の駅が登録されないように修正

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,4 @@
 class Property < ApplicationRecord
   has_many :stations, dependent: :destroy
-  accepts_nested_attributes_for :stations, allow_destroy: true
+  accepts_nested_attributes_for :stations, reject_if: proc {|attributes| attributes['route_name'].blank? and attributes['station_name'].blank? and attributes['time'].blank?}, allow_destroy: true
 end
